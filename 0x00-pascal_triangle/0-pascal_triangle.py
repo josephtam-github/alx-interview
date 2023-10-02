2c98d8f367fa07f925e48f67c1ec96502f0175f5
= <=>
2023-10-02 05:57:39 +0100

    fix: fix index bug by seperating factorial function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 """This module contains a function prints pascal's triangle"""
 
+def fact(n):
+    """prints out the n number of factorial"""
+    if n == 0:
+        return 1
+    prod = 1
+    for i in range(1, n+1):
+        prod = prod * i
+    return prod
+
+
 def pascal_triangle(n):
-    """Return a an array of arrays containing Pascal's triangle
+    """
+    pascal triangle using python
     """
     if n <= 0:
         return []
-
-    # create list triangle
-    triangle = [[1 for i in range(j + 1)] for j in range(n)]
-
-    for row_n, row in enumerate(triangle):
-        for col in range(len(row) - 1):
-            if col is 0:  # skip [n][0] index
-                continue
-            triangle[row_n][col] = triangle[row_n - 1][col - 1] + triangle[row_n - 1][col]
-    return triangle
+    List_1 = []
+    for i in range(n):
+        List_2 = []
+        for j in range(i + 1):
+            List_2.append(fact(i)//(fact(j) * fact(i - j)))
+        List_1.append(List_2)
+    return List_1
